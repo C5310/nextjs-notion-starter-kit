eaf2517d24f3de401610a679aedaf783dbf9b154
C5310 <38401066+C5310@users.noreply.github.com>
2022-12-12 09:05:41 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'df2303cf9ad0447896c6b0e6350f1da6',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: '克拉玛的博客',
+  domain: 'https://kramer98.notion.site/df2303cf9ad0447896c6b0e6350f1da6',
+  author: '一些生活的反思，一些人生的感想。',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
